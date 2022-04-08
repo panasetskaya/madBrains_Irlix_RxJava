8ade4fa211ed6c473db3c065840ad42c411823dc
Maria Panasetskaia <mariapolikutina@gmail.com>
2022-04-08 16:41:58 +0800

    added: all basic rXJava search functionality

--- app/src/main/java/com/example/madbrains_irlix_rxjava/SearchFragment.kt
+++ app/src/main/java/com/example/madbrains_irlix_rxjava/SearchFragment.kt
@@ -1,25 +1,31 @@
 package com.example.madbrains_irlix_rxjava
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.CharacterStyle
+import android.text.style.ForegroundColorSpan
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
 import android.widget.TextView
-import android.widget.Toast
+import com.jakewharton.rxbinding4.widget.queryTextChanges
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import org.json.JSONArray
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.util.concurrent.TimeUnit
 
 class SearchFragment : Fragment() {
 
     private lateinit var textViewResult: TextView
     private lateinit var textViewNoResult: TextView
     private lateinit var searchView: SearchView
+    private lateinit var textForSearch: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,21 +34,58 @@ class SearchFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         textViewResult = view.findViewById(R.id.textViewSearchResults)
+        readJsonInRxJava()
         textViewNoResult = view.findViewById(R.id.textViewNoResults)
         searchView = view.findViewById(R.id.searchViewForWords)
-//        Observable.fromCallable {
-//            Thread.sleep(500)
-//            Toast.makeText(activity, "Прошло 500 миллисек", Toast.LENGTH_SHORT).show()
-//        }
-//            .subscribeOn(Schedulers.io())
-//            .observeOn(AndroidSchedulers.mainThread())
-//            .subscribe {
-//                textViewResult.text = "пиу"
-//            }
+        searchInRxJava(searchView)
+    }
 
+    fun searchInRxJava(searchView: SearchView) {
+        searchView.queryTextChanges()
+            .subscribeOn(AndroidSchedulers.mainThread())
+            .debounce(500, TimeUnit.MILLISECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe ({
+                val searched = it.toString()
+                highlightInString(textForSearch, searched)
+            }, {
+                Log.i("MyRes", it.toString())
+            })
+    }
+
+    fun highlightInString(s: String, searched: String) {
+        val sLow = s.lowercase()
+        val searchedLow = searched.lowercase().trim()
+        if (sLow.contains(searchedLow)) {
+            textViewResult.visibility = View.VISIBLE
+            textViewNoResult.visibility = View.GONE
+            val spannable = SpannableString(s)
+            val foregroundColor = ForegroundColorSpan(resources.getColor(R.color.teal_200))
+            val firstIndex = sLow.indexOf(searchedLow)
+            spannable.setSpan(CharacterStyle.wrap(foregroundColor), firstIndex, firstIndex+searchedLow.length, 0 )
+            textViewResult.text = spannable
+        } else {
+            textViewResult.visibility = View.GONE
+            textViewNoResult.visibility = View.VISIBLE
+        }
+    }
+
+    fun readJsonInRxJava() {
+        Observable.fromCallable {
+            parseJson()
+        }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe ({
+                textForSearch = it
+                "Текст для поиска:\n$it".also { textViewResult.text = it }
+            }, {
+                Log.i("MyRes", it.toString())
+            })
     }
 
     fun parseJson(): String {
